66bbf6ad3a0341328189840303d7ddb0c20a0c2b
nikita3006 <nikitanegi3011@gmail.com>
2024-04-01 12:34:45 +0530

    installed react-icon and started developing navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Home from './component/Home'
+
 import Navbar from './component/Navbar'
 
 const App : React.FC = () => {
   return (
     <div>
         <Navbar/>
-        <Home/>
+      
     </div>
   )
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { IoHome } from "react-icons/io5";
+import { MdOutlineContactSupport } from "react-icons/md";
+import { MdOutlineCreate } from "react-icons/md";
+import { MdConnectWithoutContact } from "react-icons/md";
+import { RxMoon } from "react-icons/rx";
+import { RxSun } from "react-icons/rx";
+
 
 const Navbar : React.FC = () => {
   return (
-    <div className='bg-red-500'>
-         Navbar
+    <div className='h-full'>
+        <div className='bg-black text-white w-16 h-[90vh] mt-10'>
+        <ul className='flex flex-col  items-center justify-around h-[60vh]'>
+          <li className=''><a href=""></a><IoHome size={32}/></li>
+          <li><a href=""></a><MdOutlineContactSupport size={32}/></li>
+          <li><a href=""></a><MdOutlineCreate size={32} /></li>
+          <li><a href=""></a><MdConnectWithoutContact  size={32}/></li>
+          <li><a href=""></a><RxMoon size={32} /></li>
+          <li><a href=""></a><RxSun  size={32} /></li>
+        </ul>
+        
+      </div>
     </div>
   )
 }
