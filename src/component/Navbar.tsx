4db6b2083108e83d3ed1f7ff760cc1cdff64326a
nikita3006 <nikitanegi3011@gmail.com>
2024-04-03 13:38:29 +0530

    craeted navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -3,25 +3,58 @@ import { IoHome } from "react-icons/io5";
 import { MdOutlineContactSupport } from "react-icons/md";
 import { MdOutlineCreate } from "react-icons/md";
 import { MdConnectWithoutContact } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+
 import { RxMoon } from "react-icons/rx";
-import { RxSun } from "react-icons/rx";
+// import { RxSun } from "react-icons/rx";
+
+import '../index.css'
 
 
 const Navbar : React.FC = () => {
   return (
-    <div className='h-full'>
-        <div className='bg-black text-white w-16 h-[90vh] mt-10'>
-        <ul className='flex flex-col  items-center justify-around h-[60vh]'>
-          <li className=''><a href=""></a><IoHome size={32}/></li>
-          <li><a href=""></a><MdOutlineContactSupport size={32}/></li>
-          <li><a href=""></a><MdOutlineCreate size={32} /></li>
-          <li><a href=""></a><MdConnectWithoutContact  size={32}/></li>
-          <li><a href=""></a><RxMoon size={32} /></li>
-          <li><a href=""></a><RxSun  size={32} /></li>
+ 
+      
+      <nav className='bg-black text-white navbar '>         
+        <ul className=' navbar-nav '>
+          <li className='logo '>
+              <a href="" className='nav-link'>      
+                  <MdOutlineKeyboardDoubleArrowRight size={32}  className='icon'/>
+                 
+                  <span className='link-text'></span>
+              </a>
+          </li>
+          <li className='nav-item'>
+              <a href=""  className='nav-link'><IoHome size={32}  className='icon'/>
+              <span className='link-text'>HOME</span>
+              </a>
+          </li>
+          <li className='nav-item'>
+              <a href=""  className='nav-link'><MdOutlineContactSupport size={32} className='icon'/>
+              <span className='link-text'>ABOUT</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a href=""  className='nav-link'><MdOutlineCreate size={32}  className='icon' />
+              <span className='link-text'>CREATE</span>
+              </a>
+          </li>    
+          <li className='nav-item'>
+              <a href=""  className='nav-link' ><MdConnectWithoutContact  size={32}  className='icon'/>
+              <span className='link-text'>CONTACT</span>
+              </a>
+            </li>     
+            <li className='nav-item'>
+              <a href=""  className='nav-link' ><RxMoon  size={32}  className='icon'/>
+              <span className='link-text'></span>
+              </a>
+            </li>
+          {/* <li className='sun'><a href=""  className='link'></a><RxSun  size={32} /></li> */}
         </ul>
-        
-      </div>
-    </div>
+     
+      </nav>
+    
+   
   )
 }
 
